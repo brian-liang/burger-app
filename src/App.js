036b23b5095baaec7ee69cb2aeea1b5ea6c24d22
brian-liang <brian.j.liang@gmail.com>
2020-04-05 15:23:12 -0600

    Added Burger Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   state = {}
@@ -7,7 +9,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>Is it working?</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
